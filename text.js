de0620fde37a54eb8558829c8c14be391ee596b2
Poojithatirunagri <poojitha51199@gmail.com>
2025-06-05 23:15:12 +0530

    hello world

--- text.js
+++ text.js
@@ -1,1 +1,16 @@
 console.log("hello")
+console.log("hwllo")
+
+/*
+git status- to view current status
+
+git add . -to add current changes to stage
+
+git commit -m "message"
+
+git push
+
+git clone https://github.com/Poojithatirunagri/Poojitha.git - to
+get remote folder in local
+
+ */
